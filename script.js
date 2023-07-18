8026e589b6a41fb563f9385beae1ca800784855b
Deepakkumar121998 <130857964+Deepakkumar121998@users.noreply.github.com>
2023-07-18 23:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function decimalToBinary(num) {
-  //Write you code here
+function decimalToBinary(decimal) {
+  if (decimal === 0) {
+    return "0";
+  }
   
+  let binary = "";
+  
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Enter a decimal number: ", (decimal) => {
+  const binary = decimalToBinary(parseInt(decimal));
+  console.log("Binary equivalent:", binary);
+  readline.close();
+});
